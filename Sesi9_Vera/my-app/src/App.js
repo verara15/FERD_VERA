95ae1edb61d9d2f1352d2d4d9880537ab33953fe
Vera <vera1@student.umn.ac.id>
2021-10-10 13:12:33 +0700

    Remove duplicated directory

--- Sesi9_Vera/my-app/src/App.js
+++ Sesi9_Vera/my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+const divStyle ={
+  backgroundColor:"#eeeeee",
+  padding:"30px",
+  fontFamily:"Arial"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div style={divStyle}>
+      <h1>Hello World</h1>
+  </div>           
   );
 }
 
